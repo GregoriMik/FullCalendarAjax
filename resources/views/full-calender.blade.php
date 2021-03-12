f4e583065111c70a700841e51b311738591507b5
Gregori Mikina <gregorimikina@gmail.com>
2021-03-12 08:29:50 +0100

    Add csrf token

--- resources/views/full-calender.blade.php
+++ resources/views/full-calender.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
+    <meta name="csrf-token" content="{{ csrf_token() }}"
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -11,11 +12,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
-    <title>Hello, world!</title>
+    <title>Calendar</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+    <h1>Calendar</h1>
+<script>
+    $(document).ready(function(){
+    });
+</script>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
